3ba64378e269bc2d9c4e98c0a193633f229f2fc3
Samuli Löf <samuli.lof@gmail.com>
2016-08-25 17:48:20 +0300

    Vertex done

--- Medieur/source/headers/Graphics/Vertex.h
+++ Medieur/source/headers/Graphics/Vertex.h
@@ -2,7 +2,15 @@
 
 #include <GL\glew.h>
 
-struct Vertex {
+struct Position {
+	float x, y;
+};
+struct Color {
+	GLubyte r, g, b, a;
+};
 
+struct Vertex {
+	Position position;
+	Color color;
 };
 
