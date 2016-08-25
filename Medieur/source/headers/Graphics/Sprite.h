42aa6cf064fbc74e983cba2d15425c95d4dcc651
Samuli Löf <samuli.lof@gmail.com>
2016-08-25 13:23:29 +0300

    Sprite basic stuff done

--- Medieur/source/headers/Graphics/Sprite.h
+++ Medieur/source/headers/Graphics/Sprite.h
@@ -9,16 +9,18 @@
 class Sprite
 {
 public:
-	Sprite(const std::string& pFilepath, const int pX, const int pY, const int pWidth, const int pHeight);
 	Sprite(const std::string& pFilepath, const Rectangle& pRectangle);
-
+	Sprite(const std::string& pFilepath, const int pX, const int pY, const int pWidth, const int pHeight)
+		: Sprite(pFilepath, Rectangle{ pX, pY, pWidth, pHeight }) {}
+	~Sprite();
 	void draw(const int pX, const int pY);
 	void draw(const int pX, const int pY, const Rectangle & pSrcRect);
 
 	Rectangle getRectangle() const { return mSourceRectangle; }
 
-protected:
+private:
+	void init();
 	Rectangle mSourceRectangle;
-	GLuint mVbo;
+	GLuint mVboId;
 };
 
--- Medieur/source/src/Graphics/Sprite.cpp
+++ Medieur/source/src/Graphics/Sprite.cpp
@@ -4,27 +4,48 @@
 
 #include "units.h"
 
-
-Sprite::Sprite(const std::string& pFilepath, int pX, int pY, int pWidth, int pHeight)
-	: Sprite(pFilepath, Rectangle{pX, pY, pWidth, pHeight})
-{
-
-}
-
 Sprite::Sprite(const std::string & pFilepath, const Rectangle & pRectangle)
 	:
-
-	mSourceRectangle(pRectangle), mVbo(0)
+	mSourceRectangle(pRectangle), mVboId(0)
 {
+	
+}
+
+Sprite::~Sprite()
+{
+	if (mVboId != 0) {
+		glDeleteBuffers(1, &mVboId);
+		mVboId = 0;
+	}
+}
+
+void Sprite::init()
+{
+	if (mVboId == 0) {
+		glGenBuffers(1, &mVboId);
+	}
+
+	float vertexData[12];
+
+	glBindBuffer(GL_ARRAY_BUFFER, mVboId);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), &vertexData[0], GL_STATIC_DRAW);
+
+	// Unbind the buffer
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void Sprite::draw(const int pX, const int pY)
 {
-	Graphics::renderTexture(
-		mSpriteSheet,
-		mSourceRectangle,
-		Rectangle(pX, pY, units::kTileSize, units::kTileSize)
-	);
+	glBindBuffer(GL_ARRAY_BUFFER, mVboId);
+	glEnableVertexAttribArray(0);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glDrawArrays(GL_TRIANGLES, 0, 6);
+
+	glDisableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
 
 void Sprite::draw(const int pX, const int pY, const Rectangle & pSourceRect)
